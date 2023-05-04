e64aaa6faf3d3f6ea141072928fae1a31622225c
Mario Meltzow <m.meltzow@tarent.de>
2023-05-04 20:43:32 +0200

    add more editor help

--- src/components/QueryEditor/QueryEditorHelp.tsx
+++ src/components/QueryEditor/QueryEditorHelp.tsx
@@ -6,18 +6,32 @@ export default function QueryEditorHelp(props: QueryEditorHelpProps<JiraQuery>):
     const examples = [
         {
             title: 'cycle time scatterplot',
-            expression: "project = 'MAEN'",
+            jqlQuery: "project = 'MAEN'",
             metric: METRICS.CYCLE_TIME,
-            label: '',
+            label: 'How many days are needed to complete the issues?',
             quantile: 85
+        },
+        {
+            title: 'throughput',
+            jqlQuery: "project = 'MAEN'",
+            metric: METRICS.THROUGHPUT,
+            label: 'How many issues are completed in one week?',
+            endStatus: 'Done'
+        },
+        {
+            title: 'work item age',
+            jqlQuery: "project = 'MAEN'",
+            metric: METRICS.WORK_ITEM_AGE,
+            label: 'Shows a current snapshot of the aging of the current issues?'
         },
     ];
 
     return (
         <div>
             <h2>Cheat Sheet</h2>
+            <p>which metric do you want to try? Please click on it and the wonder will happen</p>
             {examples.map((item, index) => (
-                <div className="cheat-sheet-item" key={index} onClick={(e) => props.onClickExample({ refId: 'A', jqlQuery: item.expression, metric: item.metric, quantile: item.quantile } as JiraQuery)}>
+                <div className="cheat-sheet-item" key={index} onClick={(e) => props.onClickExample({ refId: 'A', jqlQuery: item.jqlQuery, metric: item.metric, quantile: item.quantile, endStatus: item.endStatus } as JiraQuery)}>
                     <div className="cheat-sheet-item__title" >{item.title}</div>
                     <div className="cheat-sheet-item__label">{item.label}</div>
                 </div>
